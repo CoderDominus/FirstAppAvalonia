b49c8b2b4f0247ffd8247aaa2a3674d6155bd14e
Daniel-Lima <ldaniel9090@gmail.com>
2024-07-28 19:46:59 -0300

    new file:   .gitignore 	new file:   .vscode/launch.json 	new file:   Directory.Build.props 	new file:   FirstAppAvalonia.Android/FirstAppAvalonia.Android.csproj 	new file:   FirstAppAvalonia.Android/Icon.png 	new file:   FirstAppAvalonia.Android/MainActivity.cs 	new file:   FirstAppAvalonia.Android/Properties/AndroidManifest.xml 	new file:   FirstAppAvalonia.Android/Resources/AboutResources.txt 	new file:   FirstAppAvalonia.Android/Resources/drawable-night-v31/avalonia_anim.xml 	new file:   FirstAppAvalonia.Android/Resources/drawable-v31/avalonia_anim.xml 	new file:   FirstAppAvalonia.Android/Resources/drawable/splash_screen.xml 	new file:   FirstAppAvalonia.Android/Resources/values-night/colors.xml 	new file:   FirstAppAvalonia.Android/Resources/values-v31/styles.xml 	new file:   FirstAppAvalonia.Android/Resources/values/colors.xml 	new file:   FirstAppAvalonia.Android/Resources/values/styles.xml 	new file:   FirstAppAvalonia.Browser/FirstAppAvalonia.Browser.csproj 	new file:   FirstAppAvalonia.Browser/Program.cs 	new file:   FirstAppAvalonia.Browser/Properties/launchSettings.json 	new file:   FirstAppAvalonia.Browser/runtimeconfig.template.json 	new file:   FirstAppAvalonia.Browser/wwwroot/Logo.svg 	new file:   FirstAppAvalonia.Browser/wwwroot/app.css 	new file:   FirstAppAvalonia.Browser/wwwroot/favicon.ico 	new file:   FirstAppAvalonia.Browser/wwwroot/index.html 	new file:   FirstAppAvalonia.Browser/wwwroot/main.js 	new file:   FirstAppAvalonia.Desktop/FirstAppAvalonia.Desktop.csproj 	new file:   FirstAppAvalonia.Desktop/FirstAppAvalonia.Desktop.sln 	new file:   FirstAppAvalonia.Desktop/Program.cs 	new file:   FirstAppAvalonia.Desktop/app.manifest 	new file:   FirstAppAvalonia.iOS/AppDelegate.cs 	new file:   FirstAppAvalonia.iOS/Entitlements.plist 	new file:   FirstAppAvalonia.iOS/FirstAppAvalonia.iOS.csproj 	new file:   FirstAppAvalonia.iOS/Info.plist 	new file:   FirstAppAvalonia.iOS/Main.cs 	new file:   FirstAppAvalonia.iOS/Resources/LaunchScreen.xib 	new file:   FirstAppAvalonia.sln 	new file:   FirstAppAvalonia/App.axaml 	new file:   FirstAppAvalonia/App.axaml.cs 	new file:   FirstAppAvalonia/Assets/avalonia-logo.ico 	new file:   FirstAppAvalonia/FirstAppAvalonia.csproj 	new file:   FirstAppAvalonia/ViewLocator.cs 	new file:   FirstAppAvalonia/ViewModels/MainViewModel.cs 	new file:   FirstAppAvalonia/ViewModels/ViewModelBase.cs 	new file:   FirstAppAvalonia/Views/MainView.axaml 	new file:   FirstAppAvalonia/Views/MainView.axaml.cs 	new file:   FirstAppAvalonia/Views/MainWindow.axaml 	new file:   FirstAppAvalonia/Views/MainWindow.axaml.cs

---
+++ FirstAppAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FirstAppAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
